2abc2033acf34b9ed2aa3241dacb73487a82b0f8
lexinhu <jialna@qq.com>
2020-09-01 19:10:13 +0800

    内存溢出

---
+++ src/com/xn2001/jvm/TestJvmOutOfMemory.java
@@ -0,0 +1,22 @@
+package com.xn2001.jvm;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+
+/**
+ * 模拟内存溢出
+ */
+public class TestJvmOutOfMemory {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            StringBuilder str = new StringBuilder();
+            for (int j = 0; j < 1000; j++) {
+                str.append(UUID.randomUUID().toString());
+            }
+            list.add(str.toString());
+            System.out.println("OK");
+        }
+    }
+}
